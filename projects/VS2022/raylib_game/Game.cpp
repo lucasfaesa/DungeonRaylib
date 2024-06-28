b9bb08a110d731e69466b34c9e38ad2161fed919
Lucas Rios <lucasfaesa@gmail.com>
2024-06-28 19:20:42 -0300

    - forgot

--- projects/VS2022/raylib_game/Game.cpp
+++ projects/VS2022/raylib_game/Game.cpp
@@ -5,7 +5,7 @@ Game::Game(bool initiliazed):
 {
 	
 	//rectangle = RectangleF(Vector3{ 0.0f, 0.0f, 0.0f }, Vector3{ 15.0f, 15.0f, 15.0f }, GOLD);
-	rectangle = RectangleF(Vector3{ 0.0f, 0.0f, 0.0f }, Vector3{ 5.0f, 5.0f, 5.0f}, RED);
+	rectangle = RectangleF(Vector3{ 0.0f, 2.5f, 0.0f }, Vector3{ 5.0f, 5.0f, 5.0f}, RED);
 	//windowWidth / 4 * 2 - 60, 100, 120, 60
 	Start();
 }
@@ -22,11 +22,13 @@ void Game::InputRead()
 
 void Game::Update()
 {
+
 }
 
 void Game::Draw()
 {
 	rectangle.Draw();
+	DrawGrid(100, 1.f);
 }
 
 bool Game::IsInitialized() const
