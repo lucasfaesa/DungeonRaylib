1d1a022a86ff01c2e9111a8ed0b29b8cd24fe25e
Lucas Rios <lucasfaesa@gmail.com>
2024-07-03 20:50:27 -0300

    - implemented the mesh generation by image native from raylib, duplicated mesh generation from raylib to create meshes

--- projects/VS2022/raylib_game/LevelGenerator.cpp
+++ projects/VS2022/raylib_game/LevelGenerator.cpp
@@ -5,12 +5,15 @@ void LevelGenerator::Start()
 	
 
 	Texture2D texture = LoadTexture("../resources/cubicmap_atlas.png");    // Load map texture
-	model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;    // Set map diffuse texture
+	model1.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;    // Set map diffuse texture
+	model2.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;    // Set map diffuse texture
 
-	UnloadImage(image);     // Unload cubesmap image from RAM, already uploaded to VRAM
+	UnloadImage(image1);     // Unload cubesmap image from RAM, already uploaded to VRAM
+	UnloadImage(image2);     // Unload cubesmap image from RAM, already uploaded to VRAM
 }
 
 void LevelGenerator::Draw()
 {
-	DrawModel(model, mapPosition, 1.0f, RED);
+	DrawModel(model1, mapPosition1, 1.0f, WHITE);
+	DrawModel(model2, mapPosition2, 1.0f, WHITE);
 }
--- projects/VS2022/raylib_game/LevelGenerator.h
+++ projects/VS2022/raylib_game/LevelGenerator.h
@@ -1,6 +1,8 @@
 #pragma once
 
+
 #include "raylib.h"
+
 
 class LevelGenerator {
 
@@ -10,15 +12,23 @@ public:
 	void Draw();
 
 private:
-	Image image = LoadImage("../resources/cubicmap.png");
-	Texture2D cubicmap = LoadTextureFromImage(image);
+	Image image1 = LoadImage("../resources/cubicmap.png");
+	Texture2D cubicmap1 = LoadTextureFromImage(image1);
 
-	Mesh mesh = GenMeshCubicmap(image, Vector3{ 1.0f, 2.0f, 1.0f });
-	Model model = LoadModelFromMesh(mesh);
+	Mesh mesh1 = GenMeshCubicmap(image1, Vector3{ 1.0f, 15.0f, 1.0f });
+	Model model1 = LoadModelFromMesh(mesh1);
+
+	Image image2 = LoadImage("../resources/cubicmap2.png");
+	Texture2D cubicmap2 = LoadTextureFromImage(image2);
+
+	Mesh mesh2 = GenMeshCubicmapOnlyWhites(image2, Vector3{ 1.0f, 1.0f, 1.0f });
+	Model model2 = LoadModelFromMesh(mesh2);
 
 	Texture2D texture{};    // Load map texture
 	
-	Vector3 mapPosition = { -16.0f, 2.f, -8.0f };
+	Vector3 mapPosition1 = { -16.0f, 0.f, -8.0f };
+
+	Vector3 mapPosition2 = { -16.0f, 3.f, -8.0f };
 
 
 
